561ad5eb9f440c46f9ad58bf45df681ce315738f
Festaa <Festa.Rexhepi@digitalschool.tech>
2025-06-12 17:44:21 +0200

    commit

---
+++ lesson4/array.php
@@ -0,0 +1,28 @@
+<?php
+
+    // $sport1= "Football";
+    // $sport2= "Basketball";
+    // $sport3= "Handball";
+    // $sport4= "Voleyball";
+
+    // $sports=array('Football', 'Basketball', 'Handball','Voleyball');
+
+     $sports= ['Football', 'Basketball', 'Handball','Voleyball'];
+    // echo $sports[0];
+    // echo end($sports);
+    // echo count($sports);
+
+    // for($i =0; $i<4; $i++){
+    //     echo $sports[$i], "\n";
+    // }
+
+    $len = count($sports);
+
+    for($i =0; $i < $len; $i++){
+        echo $sports[$i], "\n";
+    }
+
+
+
+
+?>
